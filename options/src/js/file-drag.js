18c6edf13bf2f36b79dc1bd69957e100083b8bd1
yuweiran <1492681276@qq.com>
2023-09-25 01:35:02 +0800

    文件位置调整，初始化option页面

---
+++ options/src/js/file-drag.js
@@ -0,0 +1,19 @@
+// const dropZone = document.getElementById('drop-zone');
+
+// // 阻止浏览器默认的文件拖放行为
+// dropZone.addEventListener('dragover', (e) => {
+//   e.preventDefault();
+// });
+
+// // 处理文件拖入事件
+// dropZone.addEventListener('drop', (e) => {
+//   e.preventDefault();
+
+//   const files = e.dataTransfer.files; // 获取拖入的文件列表
+
+//   // 处理每个拖入的文件
+//   for (const file of files) {
+//     console.log('导入文件:', file);
+//     // 在这里可以执行处理文件的操作，比如读取文件内容或上传到服务器等
+//   }
+// });
